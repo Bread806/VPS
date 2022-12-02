bdf33395226c315abb5482778986fc32efb4109d
08360565 <08360565@me.mcu.edu.tw>
2022-12-02 16:02:47 +0800

    新增第三把武器/武器跟隨玩家
    玩家控制是測試用的，不是最後的，第三種武器可以升到8級

---
+++ Assets/Script/shooting_Sword2/shooting_Sword2_2.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting_Sword2_2 : MonoBehaviour
+{
+    float sword2_speed = 10;
+    float duration = 110;
+    Vector3 sword2_scale;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sword2_scale = transform.localScale;
+        transform.Translate(sword2_speed*Time.deltaTime,0,sword2_speed*Time.deltaTime);
+        transform.Rotate (new Vector3 (0, 540, 0) * Time.deltaTime);
+        sword2_speed+=0.1f;
+        transform.localScale = new Vector3(sword2_scale.x+0.01f,  sword2_scale.y+0.01f, sword2_scale.z+0.01f);
+        Debug.Log(duration);
+        if(sword2_speed >= duration)
+            Destroy (this.gameObject,1);
+    }
+}
