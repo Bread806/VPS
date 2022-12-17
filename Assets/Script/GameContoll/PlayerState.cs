f3ad1d05ba5d9ff4a6a8b21f72d89feef67e5126
Bread806 <bsw0210903@gmail.com>
2022-12-17 22:27:07 +0800

    GetComponent from Sword1

--- Assets/Script/GameContoll/PlayerState.cs
+++ Assets/Script/GameContoll/PlayerState.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerState : MonoBehaviour
 {
-    public GameObject[] weapon; 
+    //public GameObject[] weapon; 
+    public GameObject weapon;
     private int playerHP;
     private int playerEXP;
     // Start is called before the first frame update
@@ -12,22 +13,25 @@ public class PlayerState : MonoBehaviour
     {
         playerHP = 100;
         playerEXP = 0;
-        weapon = new GameObject[6];
-        weapon = GameObject.FindGameObjectsWithTag("Sword");
-        for (int i = 0; i < weapon.Length; i++){
-            //print (weapon[i].level);
-        }
+        //weapon.GetComponent<spawn_Sword1>();
+        //weapon = new GameObject[6];
+        //weapon = GameObject.FindGameObjectsWithTag("Sword");
+        //for (int i = 0; i < weapon.Length; i++){
+        //    //print (weapon[i].level);
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.K)){
+            level_UP();
+        }
+        print (weapon.GetComponent<spawn_Sword1>().level);
     }
 
     void level_UP(){
-        //
-
+        weapon.GetComponent<spawn_Sword1>().level += 1;
     }
 
 }
