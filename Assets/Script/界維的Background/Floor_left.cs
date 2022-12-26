d1a2ae6d7aa8c795dd70118fbd01a80d08a35629
Phoenix1258 <62420215+Phoenix1258@users.noreply.github.com>
2022-12-27 01:24:09 +0800

    場景complete

--- Assets/Script/界維的Background/Floor_left.cs
+++ Assets/Script/界維的Background/Floor_left.cs
@@ -7,25 +7,20 @@ public class Floor_left : MonoBehaviour
     [SerializeField]
     Collider bound;
     public Transform prefab;
+    int count_scene = 0; 
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other){
-        if (other.tag == "Player"){
+        if ((other.tag == "Player" )& (count_scene == 0)){
+            count_scene+=1;
             copyMySelf();
         }   
     }
     void copyMySelf(){
-        Vector3 bound_center = bound.bounds.center;
-        Vector3 extents_length = new Vector3(0,0,bound.bounds.extents.z*2);
-        Instantiate(prefab,bound_center + extents_length,transform.rotation);
+        Vector3 now_position = this.transform.position;
+        Vector3 extents_length = new Vector3(-bound.bounds.extents.x*2,0,0);
+        Instantiate(prefab,now_position + extents_length,transform.rotation);
     }
-    void Start()
-    {
-        
-    }
-
+    void Start(){}
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update(){}
 }
